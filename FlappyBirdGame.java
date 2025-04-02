8459af5b882bfdc470cc9d7f918e2fe885c888ed
perfectpawa <baotlg1005@gmail.com>
2025-04-02 13:10:22 +0700

    add bird

--- FlappyBirdGame.java
+++ FlappyBirdGame.java
@@ -1,7 +1,8 @@
+import java.awt.Image;
 import javax.swing.*;
 
 public class FlappyBirdGame extends JFrame {
-    
+
     public static final int WIDTH = 360;
     public static final int HEIGHT = 640;
 
@@ -11,19 +12,25 @@ public class FlappyBirdGame extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setLayout(null);
 
-        //add image to the background
         ImageIcon background = new ImageIcon("./flappybirdbg.png");
         JLabel backgroundLabel = new JLabel(background);
-        backgroundLabel.setLayout(new BoxLayout(backgroundLabel, BoxLayout.Y_AXIS));
-        backgroundLabel.setPreferredSize(new java.awt.Dimension(WIDTH, HEIGHT));
-        backgroundLabel.setIcon(background);
+        backgroundLabel.setBounds(0, 0, WIDTH, HEIGHT);
         add(backgroundLabel);
+
+        ImageIcon birdIcon = new ImageIcon("./flappybird.png");
+        Image scaledBird = birdIcon.getImage().getScaledInstance(34, 24, Image.SCALE_SMOOTH);
+        ImageIcon resizedBirdIcon = new ImageIcon(scaledBird);
+
+        JLabel birdLabel = new JLabel(resizedBirdIcon);
+        birdLabel.setBounds(50, HEIGHT / 2, resizedBirdIcon.getIconWidth(), resizedBirdIcon.getIconHeight());
+        backgroundLabel.add(birdLabel);
 
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new FlappyBirdGame());
+        SwingUtilities.invokeLater(FlappyBirdGame::new);
     }
 }
